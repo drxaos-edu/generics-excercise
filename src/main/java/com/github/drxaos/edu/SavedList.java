90a9c18ad4f11c06606a9eb8d9a16da569b759d6
Tatiana <yanta89@mail.ru>
2016-07-01 09:00:53 +0500

    Update SavedList.java - task7

--- src/main/java/com/github/drxaos/edu/SavedList.java
+++ src/main/java/com/github/drxaos/edu/SavedList.java
@@ -1,38 +1,76 @@
 package com.github.drxaos.edu;
 
-import java.io.File;
-import java.io.Serializable;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.AbstractList;
 
 public class SavedList<E extends Serializable> extends AbstractList<E> implements Reloadable {
 
-    public SavedList(File file) {
-    }
+    private ArrayList<E> savedList = new ArrayList<E>();
+	private File fileInst;
+	
+    public SavedList(File fileInst) {
+        if (fileInst.exists()) {
+			fileInst = fileInst;
+			reload();
+		} else {
+			fileInst = new File(fileInst.toString());
+		}
+    } // construct
 
     public void reload() throws FileOperationException {
+        if (!fileInst.exists())
+			savedList.clear();
+		try {
+			FileInputStream fileInstis = new FileInputStream(fileInst);
+			ObjectInputStream objis = new ObjectInputStream(fileInstis);
+			savedList = (ArrayList<E>) objis.readObject();
+			objis.close();
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
     }
 
     @Override
     public E get(int index) {
-        return null;
+        return savedList.get(index);
     }
 
     @Override
     public E set(int index, E element) {
-        return null;
+        savedList.set(index, element);
+		save();
+		return element;
     }
 
     @Override
     public int size() {
-        return 0;
+        return savedList.size();
     }
 
     @Override
     public void add(int index, E element) {
+        savedList.add(index, element);
+		save();
     }
 
     @Override
     public E remove(int index) {
-        return null;
+        E element = savedList.get(index);
+		savedList.remove(index);
+		save();
+		return element;
     }
+    
+    private void save() {
+		try {
+			FileOutputStream fileInstFos = new FileOutputStream(fileInst);
+			ObjectOutputStream objOs = new ObjectOutputStream(fileInstFos);
+			objos.writeObject(savedList);
+			fileInstos.close();
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
+	} // save file
+	
 }
