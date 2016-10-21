0e2b2d905febd4d150164fbd9d2a0acc3fc56c15
7yon <simenkovadarya@gmail.com>
2016-10-21 11:44:34 +0500

    implemented SavedList class

--- src/main/java/com/github/drxaos/edu/SavedList.java
+++ src/main/java/com/github/drxaos/edu/SavedList.java
@@ -1,38 +1,78 @@
 package com.github.drxaos.edu;
 
-import java.io.File;
-import java.io.Serializable;
+import java.io.*;
 import java.util.AbstractList;
+import java.util.ArrayList;
 
 public class SavedList<E extends Serializable> extends AbstractList<E> implements Reloadable {
 
+    private ArrayList<E> array;
+    private File file;
+
     public SavedList(File file) {
+        this.file = file;
+
+        if (file == null)
+            file = new File("./list.dat");
+        reload();
     }
 
     public void reload() throws FileOperationException {
+        if (!file.exists()){
+            array = new ArrayList<E>();
+            return;
+        }
+
+        array = new ArrayList<E>();
+        try {
+            FileInputStream inputFile = new FileInputStream(file);
+            ObjectInputStream inputObject = new ObjectInputStream(inputFile);
+            array = (ArrayList<E>)inputObject.readObject();
+            inputObject.close();
+
+        } catch (Exception e) {
+            throw new FileOperationException(e);
+        }
     }
 
     @Override
     public E get(int index) {
-        return null;
+        if(array.isEmpty())
+            return  null;
+        else return  array.get(index);
     }
 
     @Override
     public E set(int index, E element) {
-        return null;
+        E item = array.set(index, element);
+        save();
+        return item;
     }
 
     @Override
     public int size() {
-        return 0;
+        return  array.size();
     }
 
     @Override
     public void add(int index, E element) {
+        array.add(index, element);
+        save();
+    }
+
+    private void save(){
+        try {
+            FileOutputStream outputFile = new FileOutputStream(file);
+            ObjectOutputStream outputObject = new ObjectOutputStream(outputFile);
+            outputObject.writeObject(array);
+            outputObject.close();
+        } catch (Exception e) {}
     }
 
     @Override
     public E remove(int index) {
-        return null;
+        E item = array.remove(index);
+        save();
+        return  item;
     }
 }
