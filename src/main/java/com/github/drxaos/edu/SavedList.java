5e391f1a9d254d6d3a3bffbd277e1abb2fd20b9d
alexey_deyneko <alfire17496@gmail.com>
2016-11-18 16:58:34 +0500

    done

--- src/main/java/com/github/drxaos/edu/SavedList.java
+++ src/main/java/com/github/drxaos/edu/SavedList.java
@@ -2,37 +2,95 @@ package com.github.drxaos.edu;
 
 import java.io.File;
 import java.io.Serializable;
-import java.util.AbstractList;
+import java.util.*;
+import java.io.*;
+
 
 public class SavedList<E extends Serializable> extends AbstractList<E> implements Reloadable {
+    private File file = null;
+    private List list = null;
+
+
 
     public SavedList(File file) {
+        this.list = new ArrayList();
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+                this.file = file;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        else {
+            this.file = file;
+            this.reload();
+        }
     }
 
     public void reload() throws FileOperationException {
+        this.list.clear();
+        FileInputStream fis = null;
+        ObjectInputStream ois = null;
+        try {
+            fis = new FileInputStream(this.file.getName());
+            ois = new ObjectInputStream(fis);
+            this.list = (List) ois.readObject();
+            ois.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public E get(int index) {
-        return null;
+        return (E) this.list.get(index);
     }
 
     @Override
     public E set(int index, E element) {
-        return null;
+        E oldObj = this.get(index);
+        this.list.set(index, element);
+        this.rewrite();
+        return oldObj;
     }
 
     @Override
     public int size() {
-        return 0;
+        return this.list.size();
     }
 
     @Override
     public void add(int index, E element) {
+        this.list.add(index, element);
+        this.rewrite();
     }
 
     @Override
     public E remove(int index) {
-        return null;
+        E obj = (E) this.list.remove(index);
+        this.rewrite();
+        return obj;
+    }
+
+
+
+    public void rewrite() throws FileOperationException{
+        FileOutputStream fos = null;
+        ObjectOutputStream oos = null;
+        try {
+            fos = new FileOutputStream(this.file.getName());
+            oos = new ObjectOutputStream(fos);
+            oos.writeObject(this.list);
+            oos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
